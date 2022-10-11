a5ae9c853d9a7f864a529cac3456603a46928110
AndreaGallini <andrea.gallini01@libero.it>
2022-10-11 15:44:47 +0200

    final adjustment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 const btn = document.querySelector('button');
-const email = document.getElementById('email').value;
+const email = document.getElementById('email');
+const btnreset = document.getElementById('riverifica')
 console.log(email)
 const emailDiInvitati = ['andreagallini@libero.it','pippobaudo@gmail.com','lamiaemail@hotmail.com'];
 const invited= []
+const invitato = document.querySelector('.invited')
+const nonInvitato = document.querySelector('.not-invited')
 for(i = 0 ; i < emailDiInvitati.length; i++){
     console.log(emailDiInvitati[i])  
 }
@@ -15,10 +18,17 @@ const verify = function(){
         }
     }
     if(check){
-        alert('c')
+        invitato.classList.remove('d-none')
+       nonInvitato.classList.add('d-none')
+        
     }else{
-        alert('a')
-        console.log(email)
+        invitato.classList.add('d-none')
+        nonInvitato.classList.remove('d-none')
+        btnreset.classList.remove('d-none')
     }
 }
 btn.addEventListener('click' , verify)
+const ricarica = function(){
+    window.location.reload();
+}
+btnreset.addEventListener('click' , ricarica)
